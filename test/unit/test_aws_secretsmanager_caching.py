1f7080bf3ba8711ba34c88530d14815d4a5f7a53
shailykhamesra <shaily.khamesra@squadiq.in>
2024-07-12 12:33:28 +0530

    [CU - ] Updated source code

---
+++ test/unit/test_aws_secretsmanager_caching.py
@@ -0,0 +1,206 @@
+# Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You
+# may not use this file except in compliance with the License. A copy of
+# the License is located at
+#
+# http://aws.amazon.com/apache2.0/
+#
+# or in the "license" file accompanying this file. This file is
+# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+# ANY KIND, either express or implied. See the License for the specific
+# language governing permissions and limitations under the License.
+"""
+Unit test suite for high-level functions in aws_secretsmanager_caching
+"""
+import unittest
+
+import botocore
+import pytest
+from botocore.exceptions import ClientError, NoRegionError
+from botocore.stub import Stubber
+
+from aws_secretsmanager_caching.config import SecretCacheConfig
+from aws_secretsmanager_caching.secret_cache import SecretCache
+
+pytestmark = [pytest.mark.unit, pytest.mark.local]
+
+
+class TestAwsSecretsManagerCaching(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def get_client(self, response={}, versions=None, version_response=None):
+        client = botocore.session.get_session().create_client(
+            'secretsmanager', region_name='us-west-2')
+
+        stubber = Stubber(client)
+        expected_params = {'SecretId': 'test'}
+        if versions:
+            response['VersionIdsToStages'] = versions
+        stubber.add_response('describe_secret', response, expected_params)
+        if version_response is not None:
+            stubber.add_response('get_secret_value', version_response)
+        stubber.activate()
+        return client
+
+    def tearDown(self):
+        pass
+
+    def test_default_session(self):
+        try:
+            cache = SecretCache()
+            user_agent_extra = f"AwsSecretCache/{cache.__version__}"
+            user_agent = cache._client.meta.config.user_agent
+
+            self.assertTrue(user_agent.find(user_agent_extra) > 0,
+                            f"User agent: {user_agent} ; \
+                            does not include: {user_agent_extra}")
+        except NoRegionError:
+            pass
+
+    def test_client_stub(self):
+        SecretCache(client=self.get_client())
+
+    def test_get_secret_string_none(self):
+        cache = SecretCache(client=self.get_client())
+        self.assertIsNone(cache.get_secret_string('test'))
+
+    def test_get_secret_string_missing(self):
+        response = {}
+        versions = {
+            '01234567890123456789012345678901': ['AWSCURRENT']
+        }
+        version_response = {'Name': 'test'}
+        cache = SecretCache(client=self.get_client(response,
+                                                   versions,
+                                                   version_response))
+        self.assertIsNone(cache.get_secret_string('test'))
+
+    def test_get_secret_string_no_current(self):
+        response = {}
+        versions = {
+            '01234567890123456789012345678901': ['NOTCURRENT']
+        }
+        version_response = {'Name': 'test'}
+        cache = SecretCache(client=self.get_client(response,
+                                                   versions,
+                                                   version_response))
+        self.assertIsNone(cache.get_secret_string('test'))
+
+    def test_get_secret_string_no_versions(self):
+        response = {'Name': 'test'}
+        cache = SecretCache(client=self.get_client(response))
+        self.assertIsNone(cache.get_secret_string('test'))
+
+    def test_get_secret_string_empty(self):
+        response = {}
+        versions = {
+            '01234567890123456789012345678901': ['AWSCURRENT']
+        }
+        version_response = {}
+        cache = SecretCache(client=self.get_client(response,
+                                                   versions,
+                                                   version_response))
+        self.assertIsNone(cache.get_secret_string('test'))
+
+    def test_get_secret_string(self):
+        secret = 'mysecret'
+        response = {}
+        versions = {
+            '01234567890123456789012345678901': ['AWSCURRENT']
+        }
+        version_response = {'SecretString': secret}
+        cache = SecretCache(client=self.get_client(response,
+                                                   versions,
+                                                   version_response))
+        for _ in range(10):
+            self.assertEqual(secret, cache.get_secret_string('test'))
+
+    def test_get_secret_string_refresh(self):
+        secret = 'mysecret'
+        response = {}
+        versions = {
+            '01234567890123456789012345678901': ['AWSCURRENT']
+        }
+        version_response = {'SecretString': secret}
+        cache = SecretCache(
+            config=SecretCacheConfig(secret_refresh_interval=1),
+            client=self.get_client(response,
+                                   versions,
+                                   version_response))
+        for _ in range(10):
+            self.assertEqual(secret, cache.get_secret_string('test'))
+
+    def test_get_secret_string_stage(self):
+        secret = 'mysecret'
+        response = {}
+        versions = {
+            '01234567890123456789012345678901': ['AWSCURRENT']
+        }
+        version_response = {'SecretString': secret}
+        cache = SecretCache(client=self.get_client(response,
+                                                   versions,
+                                                   version_response))
+        for _ in range(10):
+            self.assertEqual(secret, cache.get_secret_string('test',
+                                                             'AWSCURRENT'))
+
+    def test_get_secret_string_multiple(self):
+        cache = SecretCache(client=self.get_client())
+        for _ in range(100):
+            self.assertIsNone(cache.get_secret_string('test'))
+
+    def test_get_secret_binary(self):
+        secret = b'01010101'
+        response = {}
+        versions = {
+            '01234567890123456789012345678901': ['AWSCURRENT']
+        }
+        version_response = {'SecretBinary': secret}
+        cache = SecretCache(client=self.get_client(response,
+                                                   versions,
+                                                   version_response))
+        for _ in range(10):
+            self.assertEqual(secret, cache.get_secret_binary('test'))
+
+    def test_get_secret_binary_no_versions(self):
+        cache = SecretCache(client=self.get_client())
+        self.assertIsNone(cache.get_secret_binary('test'))
+
+    def test_refresh_secret_now(self):
+        secret = 'mysecret'
+        response = {}
+        versions = {
+            '01234567890123456789012345678901': ['AWSCURRENT']
+        }
+        version_response = {'SecretString': secret}
+        cache = SecretCache(client=self.get_client(response,
+                                                   versions,
+                                                   version_response))
+        secret = cache._get_cached_secret('test')
+        self.assertIsNotNone(secret)
+
+        old_refresh_time = secret._next_refresh_time
+
+        secret = cache._get_cached_secret('test')
+        self.assertTrue(old_refresh_time == secret._next_refresh_time)
+
+        cache.refresh_secret_now('test')
+
+        secret = cache._get_cached_secret('test')
+
+        new_refresh_time = secret._next_refresh_time
+        self.assertTrue(new_refresh_time > old_refresh_time)
+
+    def test_get_secret_string_exception(self):
+        client = botocore.session.get_session().create_client(
+            'secretsmanager', region_name='us-west-2')
+
+        stubber = Stubber(client)
+        cache = SecretCache(client=client)
+        for _ in range(3):
+            stubber.add_client_error('describe_secret')
+            stubber.activate()
+            self.assertRaises(ClientError, cache.get_secret_binary, 'test')
---
+++ test/unit/test_items.py
@@ -0,0 +1,161 @@
+# Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You
+# may not use this file except in compliance with the License. A copy of
+# the License is located at
+#
+# http://aws.amazon.com/apache2.0/
+#
+# or in the "license" file accompanying this file. This file is
+# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+# ANY KIND, either express or implied. See the License for the specific
+# language governing permissions and limitations under the License.
+"""
+Unit test suite for items module
+"""
+import unittest
+from datetime import timezone, datetime, timedelta
+from unittest.mock import Mock
+
+from aws_secretsmanager_caching.cache.items import SecretCacheObject, SecretCacheItem
+from aws_secretsmanager_caching.config import SecretCacheConfig
+
+
+class TestSecretCacheObject(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    class TestObject(SecretCacheObject):
+
+        def __init__(self, config, client, secret_id):
+            super(TestSecretCacheObject.TestObject, self).__init__(config, client, secret_id)
+
+        def _execute_refresh(self):
+            super(TestSecretCacheObject.TestObject, self)._execute_refresh()
+
+        def _get_version(self, version_stage):
+            return super(TestSecretCacheObject.TestObject, self)._get_version(version_stage)
+
+    def test_simple(self):
+        sco = TestSecretCacheObject.TestObject(SecretCacheConfig(), None, None)
+        self.assertIsNone(sco.get_secret_value())
+
+    def test_simple_2(self):
+        sco = TestSecretCacheObject.TestObject(SecretCacheConfig(), None, None)
+        self.assertIsNone(sco.get_secret_value())
+        sco._exception = Exception("test")
+        self.assertRaises(Exception, sco.get_secret_value)
+
+    def test_refresh_now(self):
+        config = SecretCacheConfig()
+
+        client_mock = Mock()
+        client_mock.describe_secret = Mock()
+        client_mock.describe_secret.return_value = "test"
+        secret_cache_item = SecretCacheItem(config, client_mock, None)
+        secret_cache_item._next_refresh_time = datetime.now(timezone.utc) + timedelta(days=30)
+        secret_cache_item._refresh_needed = False
+        self.assertFalse(secret_cache_item._is_refresh_needed())
+
+        old_refresh_time = secret_cache_item._next_refresh_time
+        self.assertTrue(old_refresh_time > datetime.now(timezone.utc) + timedelta(days=29))
+
+        secret_cache_item.refresh_secret_now()
+        new_refresh_time = secret_cache_item._next_refresh_time
+
+        ttl = config.secret_refresh_interval
+
+        # New refresh time will use the ttl and will be less than the old refresh time that was artificially set a month ahead
+        # The new refresh time will be between now + ttl and now + (ttl / 2) if the secret was immediately refreshed
+        self.assertTrue(new_refresh_time < old_refresh_time and new_refresh_time < datetime.now(timezone.utc) + timedelta(ttl))
+
+    def test_datetime_fix_is_refresh_needed(self):
+        secret_cached_object = TestSecretCacheObject.TestObject(SecretCacheConfig(), None, None)
+
+        # Variable values set in order to be able to test modified line with assert statement (False is not None)
+        secret_cached_object._next_retry_time = datetime.now(tz=timezone.utc)
+        secret_cached_object._refresh_needed = False
+        secret_cached_object._exception = not None
+
+        self.assertTrue(secret_cached_object._is_refresh_needed())
+
+    def test_datetime_fix_refresh(self):
+        exp_factor = 11
+
+        secret_cached_object = SecretCacheObject(
+            SecretCacheConfig(exception_retry_delay_base=1, exception_retry_growth_factor=2),
+            None, None
+        )
+        secret_cached_object._set_result = Mock(side_effect=Exception("exception used for test"))
+        secret_cached_object._refresh_needed = True
+        secret_cached_object._exception_count = exp_factor  # delay = min(1*(2^exp_factor) = 2048, 3600)
+
+        t_before = datetime.now(tz=timezone.utc)
+        secret_cached_object._SecretCacheObject__refresh()
+        t_after = datetime.now(tz=timezone.utc)
+
+        t_before_delay = t_before + timedelta(
+            milliseconds=secret_cached_object._config.exception_retry_delay_base * (
+                secret_cached_object._config.exception_retry_growth_factor ** exp_factor
+            )
+        )
+        self.assertLessEqual(t_before_delay, secret_cached_object._next_retry_time)
+
+        t_after_delay = t_after + timedelta(
+            milliseconds=secret_cached_object._config.exception_retry_delay_base * (
+                secret_cached_object._config.exception_retry_growth_factor ** exp_factor
+            )
+        )
+        self.assertGreaterEqual(t_after_delay, secret_cached_object._next_retry_time)
+
+
+class TestSecretCacheItem(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_datetime_fix_SCI_init(self):
+        config = SecretCacheConfig()
+        t_before = datetime.now(tz=timezone.utc)
+        secret_cache_item = SecretCacheItem(config, None, None)
+        t_after = datetime.now(tz=timezone.utc)
+
+        self.assertGreaterEqual(secret_cache_item._next_refresh_time, t_before)
+        self.assertLessEqual(secret_cache_item._next_refresh_time, t_after)
+
+    def test_datetime_fix_refresh_needed(self):
+        config = SecretCacheConfig()
+        secret_cache_item = SecretCacheItem(config, None, None)
+
+        # Variable values set in order to be able to test modified line with assert statement (False is not None)
+        secret_cache_item._refresh_needed = False
+        secret_cache_item._exception = False
+        secret_cache_item._next_retry_time = None
+
+        self.assertTrue(secret_cache_item._is_refresh_needed())
+
+    def test_datetime_fix_execute_refresh(self):
+        client_mock = Mock()
+        client_mock.describe_secret = Mock()
+        client_mock.describe_secret.return_value = "test"
+
+        config = SecretCacheConfig()
+        secret_cache_item = SecretCacheItem(config, client_mock, None)
+
+        t_before = datetime.now(tz=timezone.utc)
+        secret_cache_item._execute_refresh()
+        t_after = datetime.now(tz=timezone.utc)
+
+        # Make sure that the timezone is correctly set
+        self.assertEqual(secret_cache_item._next_refresh_time.tzinfo, timezone.utc)
+
+        # Check that secret_refresh_interval addition works as intended
+        self.assertGreaterEqual(secret_cache_item._next_refresh_time, t_before)
+        t_max_after = t_after + timedelta(seconds=config.secret_refresh_interval)
+        self.assertLessEqual(secret_cache_item._next_refresh_time, t_max_after)
